6bec79871e4adc66739b5f5ca15018a1d8a84608
Joshua Schmidlkofer <joshland@ashbyte.com>
2026-02-15 01:05:02 -0800

    fixes: - unify write behavior due to python 3.14 write bug. - rename backup_hosts.csv to prevent accidental commit of secrets - add typer and seamless CLI handling for cron support. - update README for new features.

--- vendor_backups/cisco_ios.py
+++ vendor_backups/cisco_ios.py
@@ -1,6 +1,6 @@
-import os
 from netmiko import ConnectHandler
 from datetime import datetime
+from .lib import write_backup
 
 # Current time and formats it to the North American time of Month, Day, and Year.
 now = datetime.now()
@@ -27,16 +27,10 @@ def backup(host, username, password, enable_secret):
         hostname = hostname[1]
     # Gets the running configuration.
     output = net_connect.send_command("show run")
-    
     # Creates the file name, which is the hostname, and the date and time.
     fileName = f"{hostname}_{dt_string}"
     # Creates the text file in the backup-config folder with the special name, and writes to it.
-    backup_file = os.path.join("backup-config", fileName)
-    with open(backup_file, "w") as backupFile:
-        backupFile.write(output)
-        pass
-
-    print(f"Outputted to {len(output)} bytes to {backup_file}")
+    write_backup(fileName, output)
     # For the GUI
     global gui_filename_output
     gui_filename_output = fileName
