d050ec0a663a45d7f07cb222b0a92c3be9a73786
Joshua Schmidlkofer <joshland@ashbyte.com>
2026-02-15 19:46:09 -0800

    Update: 1.0.2

--- router_backup/__init__.py
+++ router_backup/__init__.py
@@ -1,6 +1,6 @@
 """Router Backup - Multi-vendor network device backup tool with git-based storage."""
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 from router_backup.storage_git import StorageGit
 from router_backup.storage_pygit import StoragePyGit
