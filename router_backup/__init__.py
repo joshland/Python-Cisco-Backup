816968add47ac02312eb83469ccc815a2eff1398
Joshua Schmidlkofer <joshland@ashbyte.com>
2026-02-16 18:39:44 -0800

    Update: add listing device support

--- router_backup/__init__.py
+++ router_backup/__init__.py
@@ -1,6 +1,6 @@
 """Router Backup - Multi-vendor network device backup tool with git-based storage."""
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from router_backup.storage_git import StorageGit
 from router_backup.storage_pygit import StoragePyGit
